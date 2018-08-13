8be0af2197c15c0c552bd1e21e4d36ee8f361f8c
Kayler C <kmcookhorn@gmail.com>
2018-08-13 11:59:06 -0400

    all erb files tested and connected

--- server.rb
+++ server.rb
@@ -1,1 +1,21 @@
 # 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
+require 'sinatra'
+require 'curb'
+
+
+get '/' do
+  erb :home
+end
+
+
+get '/cookies' do
+  erb :cookies
+end
+
+get '/cakes' do
+  erb :cakes
+end
+
+get '/muffins' do
+  erb :muffins
+end
