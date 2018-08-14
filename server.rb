a95b02aa48301d0ee51b33d1479d461f769d5727
Kayler C <kmcookhorn@gmail.com>
2018-08-14 00:45:21 -0400

    class instances and some images added

--- server.rb
+++ server.rb
@@ -11,8 +11,42 @@ class Cookie
   end
 end
 
-oatmeal = Cookie.new('Oatmeal', '$2.50', 'yummy oatmeal cooke!')
+oatmeal = Cookie.new('Oatmeal Cookie', '$2.50', 'Our signature classic!  Organic oatmeal, juicy raisins and a hint of cinnamon')
+chocolate = Cookie.new('Chocolate Chip Cookie', '$2.50', 'Our top seller!! Soft baked with semi-sweet chololate morsels')
+snick = Cookie.new('Snickerdoodle Cookie', '$2.50', 'Sugar cookie topped with cinnamon')
 
+
+
+
+class Muffin
+  attr_accessor :name, :price, :description
+  def initialize(name, price, description)
+    @name = name
+    @price = price
+    @description = description
+  end
+end
+
+blue = Muffin.new('Blueberry', '$5.50', '')
+orcran = Muffin.new('Orange-Cranberry', '$5.50' '')
+banana = Muffin.new('Banana-Walnut', '$5.50', '')
+
+
+
+class Cake
+  attr_accessor :name, :price, :description
+  def initialize(name, price, description)
+    @name = name
+    @price = price
+    @description = description
+  end
+end
+
+
+strawberry = Cake.new('Strawberry-Swirl Cheesecake', '$15.00', 'Rich velvetycheesecake with organic strawberry puree swirl, on a graham cracker crust ')
+confetti = Cake.new('Confetti Celebration', '$30', 'One of our best-selllers!  This vanilla based cake with confetti sprinkles is sure to get the party started!')
+chocolate = Cake.new('Double Chocolate', '$35', 'Chocolate lovers, this one is for you!  MOist chocolate layers separated by rich chocolate frosting' )
+)
 
 
 get '/home' do
@@ -38,17 +72,4 @@ get '/events' do
   @events = JSON.parse(@data)
   @events_data = @events['events']
   erb :events
-end
-
-
-
-
-
-class Muffin
-  attr_accessor :name, :price, :description
-end
-
-
-class Cake
-  attr_accessor :name, :price, :description
 end
